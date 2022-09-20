0fd60754adcc864b23174c89d9a7c8f6cda7f251
nickith21 <92686124+nickith21@users.noreply.github.com>
2022-09-19 23:15:26 -0400

    add nfl logos

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Square = ({ id, newState }) => {
   const [status, setStatus] = React.useState(null);
   const [empty, setEmpty] = React.useState(true);
   const teamImg = [
-    <img src="./img/eagles.png"></img>,
-    <img src="./img/patriots.png"></img>,
+    <img src="./img/NFL Logos/los-angeles-rams-2020-logo.png"></img>,
+    <img src="./img/NFL Logos/nfl-atlanta-falcons-team-logo-2-300x300-2.png"></img>,
   ];
 
   let palet = ["red", "blue", "green"];
@@ -82,6 +82,38 @@ const Board = () => {
         </div>
       </div>
       <h1 onClick={refreshPage}>Restart</h1>
+      <img src="./img/NFL Logos/los-angeles-rams-2020-logo-300x300.png" />
+      <img src="./img/NFL Logos/nfl-arizona-cardinals-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-atlanta-falcons-team-logo-2-300x300-2.png" />
+      <img src="./img/NFL Logos/nfl-baltimore-ravens-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-buffalo-bills-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-carolina-panthers-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-chicago-bears-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-cincinnati-bengals-team-logo-300x300.png" />
+      <img src="./img/NFL Logos/nfl-cleveland-browns-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-dallas-cowboys-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-denver-broncos-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-detroit-lions-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-green-bay-packers-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-houston-texans-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-indianapolis-colts-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-jacksonville-jaguars-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-kansas-city-chiefs-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-los-angeles-chargers-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-miami-dolphins-logo-2018-300x300.png" />
+      <img src="./img/NFL Logos/nfl-minnesota-vikings-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-new-england-patriots-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-new-orleans-saints-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-new-york-giants-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-new-york-jets-team-logo-300x300.png" />
+      <img src="./img/NFL Logos/nfl-oakland-raiders-team-logo-300x300.png" />
+      <img src="./img/NFL Logos/nfl-philadelphia-eagles-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-pittsburgh-steelers-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-san-francisco-49ers-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-seattle-seahawks-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/nfl-tennessee-titans-team-logo-2-300x300.png" />
+      <img src="./img/NFL Logos/tampa-bay-buccaneers-2020-logo-300x300.png" />
+      <img src="./img/NFL Logos/washington-commanders-logo-300x300.png" />
     </>
   );
 };
