ea407ebc6ef81ee3f863669c24599c130672a0ed
nickith21 <92686124+nickith21@users.noreply.github.com>
2022-09-19 23:29:41 -0400

    add main logo

---
+++ teamSelector.js
@@ -0,0 +1,13 @@
+const TeamSelector = () => {
+  return (
+    <>
+      <div>
+        <img
+          className="main-logo"
+          src="./img/NFL Logos/nfl-league-logo-300x300.png"
+        ></img>
+      </div>
+      <NflTeams />
+    </>
+  );
+};
